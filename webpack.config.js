6d0ffaa3eb1c5dd422488a247946ed57881c0bf9
rharris529 <jordandine@gmail.com>
2022-05-21 06:03:16 -0500

    added image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,32 @@ const config = {
     },
     output: {
         path: path.join(__dirname + "/dist"),
-        filename: '[name].bundle.js'
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jpg$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name (file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace('../', "/assets")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
